34975ee915f44544d6e63c5a5350a8dfd2d76d25
MukhamadTaufikurRachman <m.taufikur.95@gmail.com>
2023-02-16 17:31:47 +0700

    craete seeders for users data and add bcrypt for user password

---
+++ backend/seeders/20230216102749-SeedUsersData.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { createHashFromPass } = require("../helpers/bcrypt");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    let data = require("../data/users.json");
+
+    data = data.map((el) => {
+      el.password = createHashFromPass(el.password);
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+
+      return el;
+    });
+
+    await queryInterface.bulkInsert("Users", data);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
